728d57ccc29de9fb7b49dfbc414791e24a13cf0d
ashtyn3 <ashtynmorel@gmail.com>
2025-01-01 21:07:05 -0500

    fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
-//import { createClient } from "tigerbeetle-node";
-//
-//export const tb_client = createClient({
-//    cluster_id: 0n,
-//    replica_addresses: [process.env.TB_ADDRESS || "3000"],
-//});
+import { createClient } from "tigerbeetle-node";
+
+export const tb_client = createClient({
+    cluster_id: 0n,
+    replica_addresses: [process.env.TB_ADDRESS || "3000"],
+});
